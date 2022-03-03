f06dff62d7c7db136a00aef28c883f5d09e5c48d
Rowan Oliver <r.oliver.yoga@gmail.com>
2022-03-03 12:45:29 -0800

    ddb framework, errors to be worked through

--- public/db.js
+++ public/db.js
@@ -1,3 +1,22 @@
 //DB framework
 
 let db;
+const request = indexedDB.open("Budget_db", 1);
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createdObjectStore("pending", { autoIncreamt:true });
+};
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+    
+    // check to confirm app is online 
+    if (navigator.online) {
+        checkDatabase();
+    }
+};
+
+request.onerror = function(event){
+    
+}
